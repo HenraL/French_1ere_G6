3ba249e21d627cdfdd68c62cf1bf8361230b17c5
HenraL <37299738+HenraL@users.noreply.github.com>
2020-08-11 10:36:40 +0200

    uploaded the files

---
+++ files/1ereG6/pdf p de clèves/Irina.bat
@@ -0,0 +1,19 @@
+color 0A
+@echo off
+cls
+echo "  ___________    ___________  "
+echo " /           \  /           \ "
+echo " \            \/            / "
+echo "  \                        /  "
+echo "   \                      /   "
+echo "    \       ._____.      /    "
+echo "     \      |Irina|     /     "
+echo "      \     |_____|    /      "
+echo "       \              /       "
+echo "        \            /        "
+echo "         \          /         "
+echo "          \        /          "
+echo "           \      /           "
+echo "            \    /            "
+echo "             \  /             "
+echo "              \/              "
---
+++ files/1ereG6/pdf p de clèves/coucou.bat
@@ -0,0 +1,9 @@
+color 0A
+echo off
+cls
+:s
+echo coucou
+REM timeout/t 1 /NOBREAK
+echo sale merde
+echo "Û"
+goto s
